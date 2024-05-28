cb5acf1f468c533ac6d97ef7d92c281f9ac2fda7
fadd00 <andhikahutama9@gmail.com>
2024-05-28 15:05:46 +0700

    membuat class seseorang

--- 2.cpp
+++ 2.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+
+class seseorang {
+    public:
+    virtual void pesan() = 0;
+    virtual void (){
+        cout << "pesan dari seseorang" << endl;
+    }
+};
 int main(){
 
 };
