21b58399b96a0e17ac02cd803d7c70a4eb3a5b09
fadd00 <andhikahutama9@gmail.com>
2024-05-28 15:43:10 +0700

    function remote lampu

---
+++ encaptulation.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+class remotelampu {
+    private:
+    string saklarNo[10];
+    public:
+    void setSaklarNo(int i, string s) { saklarNo[i] = value; }
+    string getSaklarNo(int i) { return saklarNo[i]; }
+}
